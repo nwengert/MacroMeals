cb678966e14ecf061f0af4ed72fc788963103459
Nathan Wengert <nate.weng@gmail.com>
2018-10-02 17:31:34 -0600

    first commit

--- server.js
+++ server.js
@@ -2,17 +2,21 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const PORT = 8181;
+const PORT = process.env.PORT || 8181;
 const morgan = require("morgan");
-const MEALS = [];
+const path = require("path")
+require("dotenv").config()
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use("/meals", require("./routes/meals.js"));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
-mongoose.connect('mongodb://localhost:27017/meals',{useNewUrlParser: true})  
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/meals',{useNewUrlParser: true})  
     .then(()=> console.log("Connected to MongoDB"))
     .catch(err => console.error(err));
-// app.use(logger);                 I DONT KNOW WHAT THESE ARE FOR
-// app.use(authenticate);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 app.listen(PORT, ()=> console.log(`Your server is currently running briskly on ${PORT}`))
