add1c33e22dd77ad9df2e7d558630a3feb85f27d
Nathan Wengert <nate.weng@gmail.com>
2018-10-11 13:37:46 -0600

    changed order

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const app = express();
+require("dotenv").config()
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8181;
 const morgan = require("morgan");
 const path = require("path")
-require("dotenv").config()
+
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
-app.use("/meals", require("./routes/meals.js"));
 app.use(express.static(path.join(__dirname, "client", "build")));
 
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/meals', {useNewUrlParser: true})  
-    .then(()=> console.log("Connected to MongoDB"))
-    .catch(err => console.error(err));
+.then(()=> console.log("Connected to MongoDB"))
+.catch(err => console.error(err));
+
+app.use("/meals", require("./routes/meals.js"));
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
